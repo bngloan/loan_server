34f3fd719c4db457577a49e544a3295197cd1f84
bngloan <113881159+bngloan@users.noreply.github.com>
2022-09-24 16:37:10 +0700

    add del

--- app/controllers/weburl.controller.js
+++ app/controllers/weburl.controller.js
@@ -13,14 +13,52 @@ exports.getAllWeburl = async (req, res) => {
         });
 }
 
-exports.updateWeburl = async (req, res) => {
+exports.createweburl = async (req, res) => {
+      return await weburl
+        .create(req.body)
+        .then((data) => {
+          res.status(200).send({ status: true });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send({
+            status: 500,
+            message:
+              err.message || "Some error occurred while creating the Loan.",
+          });
+        });
+      // });
     
+  };
+
+  exports.deleteweburl = (req, res) => {
+    const id = req.params.id;
+  
+    weburl
+      .destroy({
+        where: { id: id },
+      })
+      .then(() => {
+        res.status(200).send({
+          message: "Loan was deleted successfully!",
+        });
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: "Could not delete Loan with id=" + id,
+        });
+      });
+  };
+
+exports.updateWeburl = async (req, res) => {
 
         try{
             weburl.update({nameurl:req.body.facebookURL},{where:{name:"facebook"}});
             weburl.update({nameurl:req.body.websiteURL},{where:{name:"website"}});
             weburl.update({nameurl:req.body.lineURL},{where:{name:"line"}});
             weburl.update({nameurl:req.body.gmailURL},{where:{name:"gmail"}});
+            weburl.update({nameurl:req.body.headertxt},{where:{name:"headertxt"}});
+            weburl.update({nameurl:req.body.contenttxt},{where:{name:"contenttxt"}});
             res.status(200).send({
                 message: "Weburl was updated successfully."
               });
